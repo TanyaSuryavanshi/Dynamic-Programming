e608ef9111018b2d7104c6e374b1473d5dd51712
TanyaSuryavanshi <109675038+TanyaSuryavanshi@users.noreply.github.com>
2023-02-02 16:53:57 +0530

    Update Nth Fibonacci Number.cpp

--- Nth Fibonacci Number.cpp
+++ Nth Fibonacci Number.cpp
@@ -1,3 +1,5 @@
+//top down approach
+
 #include<iostream>
 #include <vector>
 using namespace std;
@@ -22,4 +24,24 @@ int main()
         }
         cout<<func(n,dp)<<endl;
         return 0;
+}
+
+//bottum up approach
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+        int n;
+        cin>>n;
+
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+                dp[i]=dp[i-1] + dp[i-2];
+        }
+        cout<<dp[n];
 }
