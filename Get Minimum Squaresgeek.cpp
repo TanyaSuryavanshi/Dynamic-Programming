f1656ec00f9ec7148fc61100bfaa4593bab1ab54
TanyaSuryavanshi <109675038+TanyaSuryavanshi@users.noreply.github.com>
2023-02-04 22:26:56 +0530

    Create Get Minimum Squaresgeek.cpp

---
+++ Get Minimum Squaresgeek.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+	public:
+	int solve(int n){
+	    if(n==0)
+	        return 0;
+	    int ans=n;
+	    for(int i=1;i*i<=n;i++){
+	        ans=min(ans,1+solve(n-i*i));
+	    }
+	    return ans;
+	}
+	int solvemem(int n,vector<int>& dp){
+	    if(n==0)
+	        return 0;
+	    if(dp[n]!=-1)
+	        return dp[n];
+	    int ans=n;
+	    for(int i=1;i*i<=n;i++){
+	        ans=min(ans,1+solvemem(n-i*i,dp));
+	    }
+	    dp[n]=ans;
+	    return dp[n];
+	}
+	int solvetab(int n){
+	    vector<int> dp(n+1,INT_MAX);
+	    dp[0]=0;
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j*j<=n;j++){
+	            if(i-j*j>=0)
+	                dp[i]=min(dp[i],1+dp[i-j*j]);
+	        }
+	    }
+	    return dp[n];
+	}
+	int MinSquares(int n)
+	{
+	    //return solve(n);
+	    //vector<int> dp(n+1,-1);
+	    //return solvemem(n,dp);
+	    return solvetab(n);
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans = ob.MinSquares(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
